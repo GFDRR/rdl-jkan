f32cf55b1ffd5be943f84362cfd2bf13c1def9e2
timwis <tim@timwis.com>
2016-02-17 21:56:13 -0500

    use deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f081c0dbb0fd7907d62f5befa426e48928ec05a1f30befca55cf3eb12d95c00b
-size 427
+oid sha256:0161b2e9c036811ad644fb4755b20e6fbee9ce977238ae472ad12460aa896a42
+size 421
