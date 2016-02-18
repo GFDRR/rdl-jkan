39fdefb00501e9b7dc3181fbbb6327db90b594f3
timwis <tim@timwis.com>
2016-02-17 20:28:02 -0500

    reset deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e75ce32cbf3a7004b185f3eb3a9575251e1c313b986c8fcd71fe27ed938dd7cc
-size 416
+oid sha256:181c0ddb6fb615406be5d3842be8f7905ebdc3b755112d08dd5fda9df18e9a1b
+size 420
