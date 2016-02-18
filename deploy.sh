d06cbaa671591a9483d30322bf87fafe5ab9f374
timwis <tim@timwis.com>
2016-02-17 21:40:13 -0500

    try test-deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c6b314a68dc22790d1de3d780400fa9d328877d485200985dc2a7fa519115f3
-size 426
+oid sha256:f081c0dbb0fd7907d62f5befa426e48928ec05a1f30befca55cf3eb12d95c00b
+size 427
---
+++ test-deploy.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7c806c6b1197696334f9328c454c7e9cabd0d2f230714c5c29cf018e11be1a94
+size 31
