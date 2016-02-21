f022a93d5f562336ff2cb5f763c9a18cb9568c52
timwis <tim@timwis.com>
2016-02-21 18:33:12 -0500

    ignore plugins

--- _plugins/debug.rb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:45a737b597fb1286d575db41401b23ded04a4457d0ad11f0b19b86ede0bfe482
-size 922
--- _plugins/generate_categories.rb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3f6b89c015a1d56b9f5f109faf5c86cbb05caa73f4546de97f2b751bb922842f
-size 896
