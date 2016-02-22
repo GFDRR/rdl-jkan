4884a9791ec2fe331d3df869763bfb73d710966b
timwis <tim@timwis.com>
2016-02-21 21:13:25 -0500

    fix search.json path in script

--- scripts/datasets.js
+++ scripts/datasets.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e8f6ba8ecb41ee2319f7fef50e51591435e7e7f855d819cc8242b49f89dc475
-size 765
+oid sha256:01f0e0f109bd785b3d8b0d668becaf95df73b65f3fcea03440f0669990c7dc9a
+size 767
