45b252b326ad174a3a7c56b3abee9ad3b37defea
timwis <tim@timwis.com>
2016-02-26 06:08:08 -0500

    collapse list groups, rename functions

--- scripts/datasets.js
+++ scripts/datasets.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c95f86d02320827068ee2b07e61920e00e0718002df513a2d52c0badbde978a0
-size 2786
+oid sha256:295324ea0db3dc306a80ae6d5dcc2b81e07afdc05819506f5ac4c6324fdc83aa
+size 3436
