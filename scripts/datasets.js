b7396f60561b382cfa5157e1ca314dc8ec560faf
timwis <tim@timwis.com>
2016-03-01 06:48:40 -0500

    add fuzzy search functionality, restructure scripts file, add comments

--- scripts/datasets.js
+++ scripts/datasets.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d25b6a986cd32edacfe92c95b0e63295db1905c7966ff5f4b8f034febf15888
-size 5087
+oid sha256:4653d374b26ebcd354645725dfd1ddd71f4d161c393ecd28fc3f2f88f0552f92
+size 6434
