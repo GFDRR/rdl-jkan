441391d41b4874e9653785dcd7f1b24376dfbdec
timwis <tim@timwis.com>
2016-03-01 07:12:53 -0500

    delete plugins, dataset_api.json

--- _plugins/group_by_with_size.rb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c1d5d45045527bd144f8a5920fbee1f06dc8612cfbef0ff4b2492599605b7424
-size 443
--- _plugins/where_contains.rb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8ffd2e1147992936b22091c42a83697e8ac25d06b4a827eeebb57bbc38e9efb9
-size 408
