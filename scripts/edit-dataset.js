bc489b49690f37a75927be792c109ab53b1a191e
timwis <tim@timwis.com>
2016-03-17 07:40:03 -0400

    add other fields to edit form, add/remove resources in form

--- scripts/edit-dataset.js
+++ scripts/edit-dataset.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e41c8eeb4e57ac64f6c87163aaeb4f3f1491d7b9d00ba013f3e79f5ad4271f52
-size 1791
+oid sha256:75404d62b162aa5a225c02c3fd57dc2b16d6d223d6c082cbd8d34125d6fbc4df
+size 2099
