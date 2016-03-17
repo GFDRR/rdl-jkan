7303048a4d9d6491be3ef428d1e42e3f3f9c5c56
timwis <tim@timwis.com>
2016-03-17 07:41:15 -0400

    use production config in auth

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4ce9aa32429c4bc3ddc576c473bfe2b3b83239794cf92e40e3cd00d009aa5db
-size 2098
+oid sha256:bcf54fbf59a4bfde1497e305edd4268907c3286056dbb5020836c9852556631f
+size 2097
