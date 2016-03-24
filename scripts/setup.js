d7b7cd96f398d3e6a3648e41570aa313a9cdf50b
timwis <tim@timwis.com>
2016-03-24 07:37:01 -0400

    add setup page. closes #30

---
+++ scripts/setup.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9222a763193ad8f8ffde743eb84ed3467fb15f0ee966851eda0dd93a1b93bb42
+size 2171
---
+++ setup.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eefe0da63430ba4aa634787e35781080d9dcb69532186027de6292329ff61e70
+size 2344
