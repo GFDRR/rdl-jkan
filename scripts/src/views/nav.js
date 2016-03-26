dedd5f83fe6cecc70acc223097a7d792026b3d9a
timwis <tim@timwis.com>
2016-03-26 19:01:34 -0400

    use bootstrap dropdown plugin

--- scripts/src/views/nav.js
+++ scripts/src/views/nav.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a6deb28e4e9bb59c4e4e8e3762ab11eb475d6e1ecb818f68a32bf3e4225c4ea
-size 823
+oid sha256:5d815b3c2e308c02ca5b2716c578b65637effef65a3cc626c36fcb95941dc3c6
+size 855
