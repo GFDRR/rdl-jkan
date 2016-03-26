65221349bcb39d66b229b7fab51b859d813e2313
timwis <tim@timwis.com>
2016-03-26 18:57:30 -0400

    reorder datasets routes

--- scripts/src/index.js
+++ scripts/src/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:149358a88f251ca01a94ba6d50a7b161c9669ee04ad6c8638e460db80132a495
+oid sha256:da6dfaa7a94c6286398332cb58469558c813e1019cc40b47b92fc113c4e63f5b
 size 2274
