4a50dec66ccaf7c0ee28c1c60ec7e36754379d9d
timwis <tim@timwis.com>
2016-03-26 17:46:41 -0400

    webpack build to dist dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03ccd87d7c0b9fb2554e987580c10e115d5f40929e1a8a73a1af87c3478e0f27
-size 369
+oid sha256:5d8d79396e1116fb50afb80736cff149723388ce6480b2ce42f613394be40a8d
+size 357
