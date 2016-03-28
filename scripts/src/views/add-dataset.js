4f5052c1355cd174073a68449727585a2864b478
timwis <tim@timwis.com>
2016-03-28 06:25:45 -0400

    prepend baseurl to success message dataset page link

--- scripts/src/views/add-dataset.js
+++ scripts/src/views/add-dataset.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a7742276ac13065d8f69d6e4b1a27c00a0ca3dfee69564a7c9269913b08d244
-size 2025
+oid sha256:ddff405df8aee68dfae7b69dafb758dbed9a8ad266e13b5bf14dc705aa441079
+size 2067
