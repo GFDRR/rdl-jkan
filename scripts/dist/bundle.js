93eb04bdd22b0d596e30b22ca05b0739fe0e46d1
timwis <tim@timwis.com>
2016-04-04 07:34:34 -0400

    build js

--- scripts/dist/bundle.js
+++ scripts/dist/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eac6c9651fdb6f73f4b7df6e4fd35ecac6b51e943a83de01a20e9f132df7efbe
-size 345672
+oid sha256:96c3ca01bf52e7f4b6975da782e0524a5409544fd664127767101783be96a358
+size 345704
