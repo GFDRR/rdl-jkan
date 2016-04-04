3d8f62794f9f482eeb5cd584b853decd6a70a69d
timwis <tim@timwis.com>
2016-04-04 07:02:17 -0400

    don't show non-collaborator message if isCollaborator isn't known yet

--- scripts/src/views/nav.js
+++ scripts/src/views/nav.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e850a20e41ce1bcd33118a54b81fa2c685ff9d29ae29964399ae5b24d33b1a8b
-size 1512
+oid sha256:6933ba33cb974738ef9e6a20341119d1813ffef68ede8986c002af4f1ffdbe7c
+size 1572
