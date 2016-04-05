dc0ddc4d5ba630c80cf900d7ee19e57ce6d6e4bc
timwis <tim@timwis.com>
2016-04-05 07:21:56 -0400

    view/edit organizaiton pages

--- scripts/src/index.js
+++ scripts/src/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e66bf9f31c140bba11bdd6f57616f82c890914e9daa127bd460656ea066b5839
-size 2664
+oid sha256:c2fb7d8e8e703b303a6b8c20fc72a487897965d66df3f768db788d910152215c
+size 3031
---
+++ scripts/src/views/organization.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6fc8bdfca3450c33f76dcb8f76b4474dc598ca88a0c1acffe7b05876f38a63ee
+size 2050
