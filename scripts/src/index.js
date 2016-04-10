0b6bb54884660d195fbc50102438783ae82f9c2b
timwis <tim@timwis.com>
2016-04-10 19:32:10 -0400

    use tabs on admin page

--- scripts/src/index.js
+++ scripts/src/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e488445a338dd26df81272923dc9cdfa393a774ab4102dc2a8b1281b1dc1437
-size 2864
+oid sha256:714a0d31b8d4e46791889437eccf201fcb54f2761b5c03fe9ede968801f38df3
+size 2890
