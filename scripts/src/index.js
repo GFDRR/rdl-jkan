8312a8522711036d9a6ef31a2243210372703856
timwis <tim@timwis.com>
2016-04-10 13:07:15 -0400

    dataset display component

--- scripts/src/index.js
+++ scripts/src/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03ef225ba32aee64bbf13d58344f82edebc727a5183bce04071cc295e9593477
-size 3059
+oid sha256:219b6947118314df09dfbc58b96dde3cc31032c1202bfc150a1631e02abe0368
+size 3270
