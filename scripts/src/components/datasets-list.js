23fe159fb495d939a02901e8ffe4fa757b8ec17d
timwis <tim@timwis.com>
2016-04-10 17:02:31 -0400

    datasets-list can be filtered by data attribute, add datasets list to organization page

--- scripts/src/components/datasets-list.js
+++ scripts/src/components/datasets-list.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21e1af6aeb310ca07f6fd4dafe9efa659fe1c0f402d6825b9fe2dc394d455a2e
-size 1965
+oid sha256:47a95eff6f303e799dc62d28328081c41cafae0a1d1b95d235c7f861175a6ded
+size 2530
