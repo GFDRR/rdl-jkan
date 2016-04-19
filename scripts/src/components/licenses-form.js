4a91f197bda2e6da3aca648b8ff81eb64a6954b3
timwis <tim@timwis.com>
2016-04-19 06:34:21 -0400

    js lint

--- scripts/src/components/licenses-form.js
+++ scripts/src/components/licenses-form.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20af0542f9afa7e28af39c7a9d50541a576b8dfcb4a1d7a393b6962515e840e5
-size 338
+oid sha256:3be29657aaa0ed4f0ff72e8f9e1cbc4cd502d54810b615e445dd488af2bd2e75
+size 336
