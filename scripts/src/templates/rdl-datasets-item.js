21c08a21bc56df90610f19b686ba67946160f002
ConnectedSystems <takuyai@gmail.com>
2021-02-11 23:52:43 +1100

    incremental minor change to info display

--- scripts/src/templates/rdl-datasets-item.js
+++ scripts/src/templates/rdl-datasets-item.js
@@ -1,13 +1,11 @@
-console.log("rdl items!")
-
 export default (data) => (
 `<rdl-dataset>
   <h3><a href="${data.url}">${data.title}</a></h3>
   Description: ${data.notes || ''}
   <br/>
-  Developed by (date): ${data.source} (${data.model_date | 'Date not supplied'})
+  Developed by (date): ${data.source} (${data.model_date || 'Date not supplied'})
   <br/>
-  Geo Coverage: ${data.geo_coverage || ''}
+  Geo Coverage: ${data.geo_coverage || 'not supplied'}
   <br />
   License: ${data.license}
 </rdl-dataset>`
