18e90f29f1700b1caa8857595a2ad8a82dae966d
ConnectedSystems <takuyai@gmail.com>
2021-02-11 23:30:32 +1100

    setting things up for rdl-datasets

---
+++ scripts/src/templates/rdl-datasets-item.js
@@ -0,0 +1,14 @@
+console.log("rdl items!")
+
+export default (data) => (
+`<rdl-dataset>
+  <h3><a href="${data.url}">${data.title}</a></h3>
+  Description: ${data.notes || ''}
+  <br/>
+  Developed by (date): ${data.source} (${data.model_date | 'Date not supplied'})
+  <br/>
+  Geo Coverage: ${data.geo_coverage || ''}
+  <br />
+  License: ${data.license}
+</rdl-dataset>`
+)
