12d2e19dbbde9302b9762baf86e7808c37aa4a94
Lydia Scarf <lydia@oldgrowth.city>
2024-06-04 10:14:03 -0400

    Revert "Drop developed by from dataset preview"
    This reverts commit 89285220a77e312750fe2be2f7d8fe8592d74b2c.

--- scripts/src/templates/rdl-datasets-item.js
+++ scripts/src/templates/rdl-datasets-item.js
@@ -13,6 +13,8 @@ export default (data) => (
 `<rdl-dataset>
   <h3><a href="${data.url}">${data.title}</a></h3>
   ${data.abstract || ''}
+  <br/><br/>
+  <strong>Developed by (date):</strong> ${data.source} (${data.model_date || 'Date not supplied'})
   <br/>
   <strong>Geo Coverage:</strong> ${data.geo_coverage.toString().replace(",", ", ") || 'Not supplied'}
   <br />
