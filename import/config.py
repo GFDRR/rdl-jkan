7aea25a12a55d01f019f41446fb8d1e76cf3d561
Lydia Scarf <lydia@oldgrowth.city>
2025-10-21 11:59:09 -0400

    Update /import/README.md

--- import/config.py
+++ import/config.py
@@ -1,4 +1,7 @@
-remote_target_branch = "rdl-0.3"
 root_dir = ".."
+# output directory
 datasets_dir = f"{root_dir}/_datasets"
+# input directory
 json_dir = f"{datasets_dir}/json"
+# for use when run with --ci flag
+remote_target_branch = "rdl-0.3"
