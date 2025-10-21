29de5f4cbae6dabfe4f8eb04942a2397aab7ab42
Lydia Scarf <lydia@oldgrowth.city>
2025-10-21 11:35:38 -0400

    Amend script to adapt to schema version
    Supports versions 0.2 and 0.3 and duplicates the
    rdl-02 JKAN schema without amending it. As the new
    schema hardens, we can make changes to rdl-03,
    but for now it's a sufficient shorter path to get
    the new rdl-03 datasets displaying in the same
    way as the rdl-02 datasets in the UI. There are
    some TODOs added for unresolved questions related
    to summarizations and other discrepancies between
    the v0.3 YAML schema and the official JSON schema.

--- import/mappers.py
+++ import/mappers.py
@@ -1,28 +1,11 @@
-import config
-
-def make_resource(resource):
-    """Convert RDL resource metadata into JKAN frontmatter"""
-    return {
-        # required; throw if missing
-        "description": resource["description"],
-        "format": resource["format"],
-        "id": resource["id"],
-        "title": resource["title"],
-        # optional
-        "coordinate_system": resource.get("coordinate_system"),
-        "download_url": resource.get("download_url", resource.get("access_url")),
-        "spatial_resolution": resource.get("spatial_resolution"),
-    }
-
-
 def make_exposure(exposure):
     """Convert RDL exposure metadata into JKAN frontmatter"""
     if exposure is None:
         return None
 
     props_to_summarize = {
-        "dimension": [], # found on metric
-        "quantity_kind": [] # found on metric
+        "dimension": [],  # found on metric
+        "quantity_kind": []  # found on metric
     }
 
     if "metrics" in exposure:
@@ -48,13 +31,13 @@ def make_hazard(hazard):
         return None
 
     props_to_summarize = {
-        "calculation_method": [], # found on event, event_set
-        "disaster_identifiers": [], # found on event
-        "hazard_analysis_type": [], # found on event_set as analysis_type
-        "hazard_type": hazard.get("type",[]), # found on hazard, event.hazard as type
-        "intensity": hazard.get("intensity_measure",[]), # found on hazard, event.hazard as intensity_measure
-        "occurrence_range": [], # found on event_set
-        "processes": hazard.get("processes",[]), # found on hazard, event.hazard
+        "calculation_method": [],  # found on event, event_set
+        "disaster_identifiers": [],  # found on event
+        "hazard_analysis_type": [],  # found on event_set as analysis_type
+        "hazard_type": hazard.get("type", []),  # found on hazard, event.hazard as type
+        "intensity": hazard.get("intensity_measure", []),  # found on hazard, event.hazard as intensity_measure
+        "occurrence_range": [],  # found on event_set
+        "processes": hazard.get("processes", []),  # found on hazard, event.hazard
     }
 
     for event_set in hazard["event_sets"]:
@@ -187,7 +170,7 @@ def make_loss(loss):
         "vulnerability_id": [],
     }
 
-    for l in loss.get("losses",[]):
+    for l in loss.get("losses", []):
         if "dimension" in l.get("cost", {}):
             props_to_summarize["dimension"].append(l["cost"]["dimension"])
         if "hazard_type" in l:
@@ -238,10 +221,27 @@ def make_loss(loss):
     }
 
 
-def make_dataset_frontmatter(dataset):
-    """Formats RDL metadata into JKAN frontmatter for a dataset"""
+# v0.2 specific mappers
+def make_resource_v02(resource):
+    """Convert RDL v0.2 resource metadata into JKAN frontmatter"""
+    return {
+        # required; throw if missing
+        "description": resource["description"],
+        "format": resource["format"],
+        "id": resource["id"],
+        "title": resource["title"],
+        # optional
+        "coordinate_system": resource.get("coordinate_system"),
+        "download_url": resource.get("download_url", resource.get("access_url")),
+        "spatial_resolution": resource.get("spatial_resolution"),
+    }
+
+
+def make_dataset_frontmatter_v02(dataset):
+    """Formats RDL v0.2 metadata into JKAN frontmatter for a dataset"""
 
     payload = {
+        "schema": "rdl-02",
         # try first; required by write_yaml
         "title": dataset["title"],
         # required; throw if missing
@@ -250,9 +250,8 @@ def make_dataset_frontmatter(dataset):
         "dataset_id": dataset["id"],
         "license": dataset["license"],
         "publisher": dataset["publisher"],
-        "resources": [make_resource(resource) for resource in dataset["resources"]],
+        "resources": [make_resource_v02(resource) for resource in dataset["resources"]],
         "risk_data_type": dataset["risk_data_type"],
-        "schema": config.schema_version,
         "spatial": dataset["spatial"],
         # optional
         "description": dataset.get("description"),
@@ -262,6 +261,98 @@ def make_dataset_frontmatter(dataset):
         "version": dataset.get("version"),
         # must include one of
         "exposure": make_exposure(dataset.get("exposure")),
+        "hazard": make_hazard(dataset.get("hazard")),
+        "loss": make_loss(dataset.get("loss")),
+        "vulnerability": make_vulnerability(dataset.get("vulnerability")),
+    }
+
+    if payload["spatial"].get("scale") == "global":
+        if "countries" in payload["spatial"] and type(payload["spatial"]["countries"]) == list:
+            payload["spatial"]["countries"].append('GLO')
+        else:
+            payload["spatial"]["countries"] = ['GLO']
+    return payload
+
+
+# v0.3 specific mappers
+def make_entity(entity):
+    return {
+        "name": entity['name'],
+        "email": entity['email'],
+        "url": entity['url'],
+    }
+
+
+def make_attribution(attribution):
+    """Convert RDL attribution metadata into JKAN frontmatter"""
+    return {
+        # required; throw if missing
+        "id": attribution["id"],
+        "role": attribution["role"],
+        "entity": make_entity(attribution["entity"]),
+    }
+
+
+def make_metric(metric):
+    return {
+        # required; throw if missing
+        "id": metric["id"],
+        "dimension": metric["dimension"],
+        "quantity_kind": metric["quantity_kind"],
+    }
+
+
+def make_period(period):
+    return {
+        "start": period.get("start"),
+        "end": period.get("end"),
+        "duration": period.get("duration"),
+        "temporal_resolution": period.get("temporal_resolution"),
+    }
+
+
+def make_resource_v03(resource):
+    """Convert RDL v0.3 resource metadata into JKAN frontmatter"""
+    return {
+        # required; throw if missing
+        "description": resource["description"],
+        "format": resource["format"],
+        "id": resource["id"],
+        "title": resource["title"],
+        # optional
+        "coordinate_system": resource.get("coordinate_system"),
+        "download_url": resource.get("download_url", resource.get("access_url")),
+        "spatial_resolution": resource.get("spatial_resolution"),
+        "media_type": resource.get("media_type"),
+        "temporal": make_period(resource.get("temporal")),
+    }
+
+def make_dataset_frontmatter_v03(dataset):
+    """Formats RDL v0.3 metadata into JKAN frontmatter for a dataset"""
+
+    payload = {
+        "schema": "rdl-03",
+        # try first; required by write_yaml
+        "title": dataset["title"],
+        # required; throw if missing
+        "contact_point": next((e for e in dataset["attributions"] if e['role'] == 'contact_point'))['entity'],
+        "creator": next((e for e in dataset["attributions"] if e['role'] == 'creator'))['entity'],
+        "publisher": next((e for e in dataset["attributions"] if e['role'] == 'publisher'))['entity'],
+        "dataset_id": dataset["id"],
+        "license": dataset["license"],
+        "resources": [make_resource_v03(resource) for resource in dataset["resources"]],
+        "risk_data_type": dataset["risk_data_type"],
+        "spatial": dataset["spatial"],
+        # optional
+        "description": dataset.get("description"),
+        "details": dataset.get("details"),
+        # TODO: how should project be summarized for rdl-03?
+        "project": dataset.get("project").get("name"),
+        "purpose": dataset.get("purpose"),
+        "version": dataset.get("version"),
+        # must include one of
+        # TODO: how should exposure be summarized for rdl-03?
+        "exposure": next(make_exposure(exposure) for exposure in dataset.get("exposure")),
         "hazard": make_hazard(dataset.get("hazard")),
         "loss": make_loss(dataset.get("loss")),
         "vulnerability": make_vulnerability(dataset.get("vulnerability")),
