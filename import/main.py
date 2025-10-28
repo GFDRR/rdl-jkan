2a296bbf13097f34279237f509ff6b51bf0fd435
Lydia Scarf <lydia@oldgrowth.city>
2025-10-28 09:51:38 -0400

    Set exit code when catching exceptions

--- import/main.py
+++ import/main.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import os
 import re
+import sys
 
 import logging
 import fnmatch
@@ -59,13 +60,14 @@ def write_to_markdown(dataset_from_json):
         clean_up_old_versions(dataset_frontmatter)
         # Write output
         utils.write_frontmatter(dataset_frontmatter, config.datasets_dir)
-
+        return 0
     except Exception as e:
         logging.error(
             f"While writing {dataset.get('title', 'a dataset with a missing title')} "
             f"(dataset_id: {dataset.get('id', 'missing')})",
             exc_info=e
         )
+        return 1
 
 
 if __name__ == "__main__":
@@ -101,18 +103,28 @@ if __name__ == "__main__":
         current_commit = repo.head.commit
         diff = current_commit.diff(f"origin/{config.remote_target_branch}")
         files = {item.a_path for item in diff}
-        for json_file in fnmatch.filter(files, '_datasets/*.json'):
+        for json_file in fnmatch.filter(files, '_datasets/json/*.json'):
             with open(os.path.join(config.root_dir, json_file), encoding='utf-8') as input_file:
                 datasets_json = json.load(input_file)
+                schema = datasets_json["schema"]
+                exit_code = 0
                 for dataset in datasets_json["datasets"]:
-                    write_to_markdown(dataset)
+                    result = write_to_markdown(dataset, schema)
+                    if result != 0:
+                        exit_code = result  
+                sys.exit(exit_code)
     elif args.type == "batch":
         input_path = Path(args.input_folder)
         for json_file in input_path.glob(f"{config.json_dir}/*.json"):
             with open(json_file, encoding='utf-8') as input_file:
                 datasets_json = json.load(input_file)
+                schema = datasets_json["schema"]
+                exit_code = 0
                 for dataset in datasets_json["datasets"]:
-                    write_to_markdown(dataset)
+                    result = write_to_markdown(dataset, schema)
+                    if result != 0:
+                        exit_code = result  
+                sys.exit(exit_code)
     else:
         raise ValueError(f"Unknown type {args.type}")
 
