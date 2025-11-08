411658dc26431d0dc16a0b2daf253e2927ff1f20
Lydia Scarf <lydia@oldgrowth.city>
2025-11-08 12:01:34 +0100

    Dynamically validate JSON metadata against listed schema
    There is a hardcoded schema link for the draft v0.3
    url that needs to be replaced when it's finalized.

--- import/main.py
+++ import/main.py
@@ -2,10 +2,12 @@
 
 import argparse
 import json
+from jsonschema import validate
 import os
 import re
 import sys
-
+import requests
+import json
 import logging
 import fnmatch
 from pathlib import Path
@@ -41,15 +43,44 @@ def clean_up_old_versions(json_dataset):
                 print(f"Deleting {filepath} with id of {markdown_dataset_id}")
                 os.remove(filepath)
 
+def fetch_schema(schema_url, schema_path):
+    response = requests.get(schema_url)
+
+    if response.status_code == 200:
+        with open(schema_path, mode='w') as file:
+            response_dict = response.json()
+            json.dump(response_dict, file)
+    else:
+        raise Exception(f"Failed to retrieve schema from {hardcoded_schema_url}, status code: {response.status_code}")
+
+
+def validate_json_with_schema(dataset_from_json, schema_url):
+    if schema_url is config.schema_url_v3:
+        schema_path = f"{config.schema_path}/rdl-03.json"
+        is_cached = os.path.isfile(schema_path)
+        if not is_cached:
+            # TODO: delete & replace hardcoded_schema_url with schema_url when v0.3 finalized
+            hardcoded_schema_url = "https://raw.githubusercontent.com/GFDRR/CCDR-tools/refs/heads/main/_static/rdls_schema_url_v3.json"
+            fetch_schema(hardcoded_schema_url, schema_path)
+    else:
+        schema_path = f"{config.schema_path}/rdl-02.json"
+        is_cached = os.path.isfile(schema_path)
+        if not is_cached:
+            fetch_schema(schema_url, schema_path)
+
+    with open(schema_path, 'r') as file:
+        schema = json.load(file)  # Load the schema from the file object    # raises exception if invalid
+        validate(instance=dataset_from_json, schema=schema)
 
 def write_to_markdown(dataset_from_json, schema):
     try:
         # Generate frontmatter
         dataset_frontmatter = None
+        validate_json_with_schema(dataset_from_json, schema)
         match schema:
-            case config.schema_v3:
+            case config.schema_url_v3:
                 dataset_frontmatter = mappers.make_dataset_frontmatter_v03(dataset_from_json)
-            case config.schema_v2:
+            case config.schema_url_v2:
                 dataset_frontmatter = mappers.make_dataset_frontmatter_v02(dataset_from_json)
             case _:
                 logging.error(
@@ -107,7 +138,7 @@ if __name__ == "__main__":
         for json_file in fnmatch.filter(files, f"_datasets/json/*.json"):
             with open(os.path.join(config.root_dir, json_file), encoding='utf-8') as input_file:
                 datasets_json = json.load(input_file)
-                schema = datasets_json["schema"]
+                schema = datasets_json.get("schema", config.schema_url_v2)
                 for dataset in datasets_json["datasets"]:
                     result = write_to_markdown(dataset, schema)
                     if result != 0:
@@ -118,7 +149,7 @@ if __name__ == "__main__":
             with open(json_file, encoding='utf-8') as input_file:
                 datasets_json = json.load(input_file)
                 
-                schema = datasets_json.get("schema", config.schema_v2)
+                schema = datasets_json.get("schema", config.schema_url_v2)
                 for dataset in datasets_json["datasets"]:
                     result = write_to_markdown(dataset, schema)
                     if result != 0:
