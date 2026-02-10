4e7e73fdb2e189b0a1ea7bc0ee81259f4c03643c
Lydia Scarf <lydia@oldgrowth.city>
2026-02-10 13:22:30 -0500

    Tweak default catalog label, sorting

--- scripts/src/components/rdl-catalog-filter.js
+++ scripts/src/components/rdl-catalog-filter.js
@@ -1,62 +1,69 @@
-import $ from 'jquery'
-import { chain, omit, defaults } from 'lodash'
+import $ from "jquery";
+import { chain, get, omit, defaults } from "lodash";
 
-import TmplListGroupItem from '../templates/list-group-item'
-import { setContent, slugify, collapseListGroup } from '../util'
-
+import TmplListGroupItem from "../templates/list-group-item";
+import { setContent, slugify, collapseListGroup } from "../util";
 
 function prep_catalog(catalog, params, datasetsForCatalog) {
-  const catalogSlug = slugify(catalog)
-  const selected = params.catalog && params.catalog === catalogSlug
-  const itemParams = selected ? omit(params, 'catalog') : defaults({ catalog: catalogSlug }, params)
+  const catalogSlug = slugify(catalog);
+  const selected = params.catalog && params.catalog === catalogSlug;
+  const itemParams = selected
+    ? omit(params, "catalog")
+    : defaults({ catalog: catalogSlug }, params);
   return {
-    title: catalog === "undefined" ? "Unknown Catalog" : catalog,
-    url: '?' + $.param(itemParams),
+    title: catalog,
+    url: "?" + $.param(itemParams),
     count: datasetsForCatalog.length,
     unfilteredCount: params.length,
-    selected: selected
-  }
+    selected: selected,
+  };
 }
 
 export default class {
   constructor(opts) {
-    const catalogs = this._catalogsWithCount(opts.datasets, opts.params)
-    var consolidated = []
+    const catalogs = this._catalogsWithCount(opts.datasets, opts.params);
+    var consolidated = [];
     catalogs.forEach(function (l) {
-      const idx = consolidated.findIndex(x => x.title === l.title)
+      const idx = consolidated.findIndex((x) => x.title === l.title);
       if (idx == -1) {
-        consolidated.push(l)
+        consolidated.push(l);
       } else {
-        consolidated[idx].count += l.count
-        consolidated[idx].unfilteredCount += l.unfilteredCount
+        consolidated[idx].count += l.count;
+        consolidated[idx].unfilteredCount += l.unfilteredCount;
       }
-    })
+    });
 
-    const catalogsMarkup = consolidated.sort((a, b) => {
-      if (a.count > b.count) return -1;
-      if (a.count < b.count) return 1;
-      if (a.count === b.count) {
-        const titleA = a.title.toUpperCase();
-        const titleB = b.title.toUpperCase();
-        if (titleA < titleB) return -1;
-        if (titleA > titleB) return 1;
-        return 0;
-      }
-    }).map(TmplListGroupItem)
-    setContent(opts.el, catalogsMarkup)
-    collapseListGroup(opts.el)
+    const catalogsMarkup = consolidated
+      .sort((a, b) => {
+        if (a.title === "Other") return 1;
+        if (b.title === "Other") return -1;
+        if (a.count > b.count) return -1;
+        if (a.count < b.count) return 1;
+        if (a.count === b.count) {
+          const titleA = a.title.toUpperCase();
+          const titleB = b.title.toUpperCase();
+          if (titleA < titleB) return -1;
+          if (titleA > titleB) return 1;
+          return 0;
+        }
+      })
+      .map(TmplListGroupItem);
+    setContent(opts.el, catalogsMarkup);
+    collapseListGroup(opts.el);
   }
 
   _catalogsWithCount(datasets, params) {
     return chain(datasets)
-      .groupBy('catalog')
+      .groupBy((dataset) =>  dataset.catalog ?? "Other")
       .flatMap(function (datasetsForCatalog, catalog) {
-        var catalogs = catalog.split(",")
-        var collated = catalogs.map(c => prep_catalog(c, params, datasetsForCatalog))
+        var catalogs = catalog.split(",");
+        var collated = catalogs.map((c) =>
+          prep_catalog(c, params, datasetsForCatalog),
+        );
 
-        return collated
+        return collated;
       })
-      .orderBy('unfilteredCount', 'desc')
-      .value()
+      .orderBy("unfilteredCount", "desc")
+      .value();
   }
 }
